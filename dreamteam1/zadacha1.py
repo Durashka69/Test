497392ffbd0d441eb131b240f5e022b5fc73358e
kantemir123 <itfls.academy@gmail.com>
2022-11-22 15:47:07 +0600

    zadacha1

---
+++ dreamteam1/zadacha1.py
@@ -0,0 +1,6 @@
+a = 333
+b = 555
+buf = a
+a = b
+b = buf
+print(a,b)
--- guljigit.py
+++
@@ -1,8 +0,0 @@
-
-Kylych = 'Granny'
-
-
-if Kylych == 'Granny':
-    print('Кылыч дед')
-else:
-    print('Кылыч все равно дед')
